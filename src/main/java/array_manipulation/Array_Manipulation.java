69d614ac8b888e225a8854d90df218163d14964a
ibrahim.alzuabi <ibrahim.alzuabi@foo-solutions.com>
2024-01-25 16:01:21 +0300

    array_manipulation

---
+++ src/main/java/array_manipulation/Array_Manipulation.java
@@ -0,0 +1,85 @@
+package array_manipulation;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+import java.util.concurrent.atomic.AtomicLong;
+
+
+class Result {
+
+    /*
+     * Complete the 'arrayManipulation' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. 2D_INTEGER_ARRAY queries
+     */
+
+    public static long arrayManipulation(int n, List<List<Integer>> queries) {
+        long[] res = new long[n + 1];
+        queries.forEach(row ->
+        {
+            int from = row.get(0) - 1;
+            int to = row.get(1);
+            int value = row.get(2);
+            res[from] = res[from] + value;
+            res[to] = res[to] - value;
+        });
+        AtomicLong sums = new AtomicLong(0);
+        AtomicLong max = new AtomicLong(0);
+        Arrays.stream(res).forEach(x -> {
+                    sums.set(sums.get() + x);
+                    if (sums.get() > max.get())
+                        max.set(sums.get());
+                }
+        );
+        return max.get();
+
+    }
+
+}
+
+class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("OUTPUT_PATH"));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int m = Integer.parseInt(firstMultipleInput[1]);
+
+        List<List<Integer>> queries = new ArrayList<>();
+
+        IntStream.range(0, m).forEach(i -> {
+            try {
+                queries.add(
+                        Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                                .map(Integer::parseInt)
+                                .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        long result = Result.arrayManipulation(n, queries);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
